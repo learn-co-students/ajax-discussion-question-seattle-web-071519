e7ad73cec0b6b79efdcfac66ae3a11eed2ca4a5e
Ellyn <eka249@nyu.edu>
2019-08-28 11:08:39 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+let btn = document.querySelector(".btn-primary")
+btn.addEventListener("click", handleAJAX)
+
+function handleAJAX(e) {
+  let data = fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then( function(data) {
+      let jsonData = data
+      document.querySelector("#cell").textContent = JSON.stringify(jsonData.results[0].cell)
+      console.log(jsonData)
+    
+    })
+  // console.log(data)
+}
